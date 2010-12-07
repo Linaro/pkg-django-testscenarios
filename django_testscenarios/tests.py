40fcac2881a4f2e4526b608007de6f1c0cab2ce0
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-12-07 22:35:59 +0100

    Fix subtle bug in TransactionTestCase inheritance
    A subtle bug was found that caused django_testscenarios.TestCase and
    django_testscenarios.TransactionTestCase (regardless of scenarios) to be a
    subclass of django.test.TestCase. This caused a subtle side effect of possible
    test failure due to the optimisation of database cleanup code done inside
    Django. That optimisation sorts all the test cases inside a test suite so that
    all tests _not_ using transactions (and hence inheriting from
    django.test.TestCase) would come first. The Django database cleanup code
    depends on this property.
    
    Due to the fixed bug this property would no longer be preserved. This caused a
    possible situation where a run of plain django.test.TestCase would be performed
    in a dirty database left over by the preceding run of
    django.test.TransactionTestCase.  The ordering of tests depends on the test
    loader iteration of a python module's dictionary so it is not deterministic.
    
    To solve the problem django_testscenarios.TestCase inherits from
    django.test.TestCase and django_testscenarios.TransactionTestCase inherits from
    django.test.TransactionTestCase and _does not_ inherits from
    django.test.TestCase
    
    Identical fix was applied to the *WithScenarios classes.

--- django_testscenarios/tests.py
+++ django_testscenarios/tests.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with django-testscenarios.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
+
 from django.db import models, transaction
 from django.test import (
     TestCase as DjangoTestCase,
@@ -190,3 +192,93 @@ class TransactionsWorkWithTestScenariosTestCaseAndSomeScenarios(
         * test scenarios test case (with transaction support)
         * two dummy scenarios so that multiple test cases get generated
     """
+
+
+class TestReorderingNotBroken(DjangoTestCase):
+    """
+    Test that test suite reordering done inside DjangoTestSuiteRunner
+    class (to optimize database setup/tear down code) is not going to
+    reorder our improved test classes.
+    """
+
+    class Plain(TestCase):
+        """" Empty test class inheriting from TestCase """
+
+        def runTest(self, result):
+            pass
+
+    class PlainScenarios(TestCaseWithScenarios):
+
+        def runTest(self, result):
+            pass
+
+    class Transaction(TransactionTestCase):
+        """" Empty test class inheriting from TransactionTestCase """
+
+        def runTest(self, result):
+            pass
+
+    class TransactionScenarios(TransactionTestCaseWithScenarios):
+        """" Empty test class inheriting from TransactionTestCase """
+
+        def runTest(self, result):
+            pass
+
+    def ensure_proper_order(self, initial_order, proper_order):
+        """
+        Create a TestSuite with test cases in initial_order, reorder
+        them using the same logic that Django applies and verify that
+        the order is proper_order
+        """
+        from django.test.simple import reorder_suite
+
+        suite = unittest.TestSuite()
+        for cls in initial_order:
+            suite.addTest(cls())
+        reordered_suite = reorder_suite(suite, (DjangoTestCase,))
+        self.assertEqual(map(type, reordered_suite._tests), proper_order)
+
+    def test_transaction_test_case_stays_last(self):
+        self.ensure_proper_order(
+            [self.Plain, self.Transaction],
+            [self.Plain, self.Transaction])
+
+    def test_transaction_test_case_is_moved_to_be_last(self):
+        self.ensure_proper_order(
+            [self.Transaction, self.Plain],
+            [self.Plain, self.Transaction])
+
+    def test_transaction_scenarios_test_case_stays_last(self):
+        self.ensure_proper_order(
+            [self.Plain, self.TransactionScenarios],
+            [self.Plain, self.TransactionScenarios])
+
+    def test_transaction_scenarios_test_case_is_moved_to_be_last(self):
+        self.ensure_proper_order(
+            [self.TransactionScenarios, self.Plain],
+            [self.Plain, self.TransactionScenarios])
+
+    def test_plain_test_case_stays_first(self):
+        self.ensure_proper_order(
+            [self.Plain, self.Transaction],
+            [self.Plain, self.Transaction])
+
+    def test_plain_test_case_is_moved_to_be_first(self):
+        self.ensure_proper_order(
+            [self.Transaction, self.Plain],
+            [self.Plain, self.Transaction])
+
+    def test_plain_test_case_stays_first(self):
+        self.ensure_proper_order(
+            [self.Plain, self.Transaction],
+            [self.Plain, self.Transaction])
+
+    def test_plain_scenarios_test_case_is_moved_to_be_first(self):
+        self.ensure_proper_order(
+            [self.Transaction, self.PlainScenarios],
+            [self.PlainScenarios, self.Transaction])
+
+    def test_plain_scenarios_test_case_stays_first(self):
+        self.ensure_proper_order(
+            [self.PlainScenarios, self.Transaction],
+            [self.PlainScenarios, self.Transaction])
