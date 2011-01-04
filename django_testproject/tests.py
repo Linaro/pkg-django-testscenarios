65320dcf8d00e21f615700651db071e43523eb79
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-01-04 01:02:12 +0100

    Fix test_labels on django 1.1

--- django_testproject/tests.py
+++ django_testproject/tests.py
@@ -7,6 +7,7 @@ def run_tests():
     os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
     from django.conf import settings
     from django.test.utils import get_runner
+    test_labels = [settings.INSTALLED_APPS[-1]]
     if django.VERSION[0:2] <= (1, 1):
         # Prior to django 1.2 the runner was a plain function
         runner_fn = get_runner(settings)
@@ -15,6 +16,5 @@ def run_tests():
         # After 1.2 the runner is a class
         runner_cls = get_runner(settings)
         runner = runner_cls(verbosity=2, interactive=False).run_tests
-        test_labels = [settings.INSTALLED_APPS[-1]]
     failures = runner(test_labels)
     sys.exit(failures)
