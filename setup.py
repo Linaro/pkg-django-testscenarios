94fd8cf118f4e2bdc63faadea72556aa19079228
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-01-19 11:25:03 +0100

    Add an ability to test more than one application with django_testproject.tests.run_tests

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name = 'django-testscenarios',
-    version = "0.5",
+    version = "0.5.1",
     author = "Zygmunt Krynicki",
     author_email = "zygmunt.krynicki@linaro.org",
     description = "Django-compatible testscenarios.TestWithScenarios",
