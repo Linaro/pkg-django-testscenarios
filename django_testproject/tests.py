402142bc7598ea3eb5d15fa4355848e895fb9e11
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-05-18 12:02:52 +0200

    Add copyright header, add run_tests_for
    Previously run_tests hard-coded the location of DJANGO_SETTINGS_MODULE, this
    proved a bad decision due to unfortunate interactions with path altering
    modules and Django fork-based double import of the actual settings module.
    
    To fix this I introduce a new function run_tests_for() that allows the caller
    to explicitly provide the settings module for their application. This allows
    for fully qualified (no more relative imports please) module as settings.

--- django_testproject/tests.py
+++ django_testproject/tests.py
@@ -1,10 +1,45 @@
+# Copyright (C) 2010, 2011 Linaro Limited
+#
+# Author: Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
+#
+# This file is part of django-testscenarios.
+#
+# django-testscenarios is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation
+#
+# django-testscenarios is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with django-testscenarios.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Helper function for running tests via setup.py test
+"""
+
 import os
 import sys
 
 import django
 
-def run_tests(test_last_n_apps=-1):
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
+
+def run_tests_for(settings_module_name, test_last_n_apps=-1):
+    """
+    Helper function that simplifies testing Django applications via setup.py
+    test
+
+    The idea is to test your application in a small test project. Since not
+    everything in your project is relevant (you don't want to test
+    django.contrib.auth gazillion times just because you use it in your
+    application) run_tests allows you to run just a subset of applications. By
+    default last item in INSTALLED_APPLICATIONS is tested. You can change it by
+    calling run_tests() with different argument.
+    """
+    os.environ['DJANGO_SETTINGS_MODULE'] = settings_module_name
+
     from django.conf import settings
     from django.test.utils import get_runner
     test_labels = settings.INSTALLED_APPS[test_last_n_apps:]
@@ -18,3 +53,11 @@ def run_tests(test_last_n_apps=-1):
         runner = runner_cls(verbosity=2, interactive=False).run_tests
     failures = runner(test_labels)
     sys.exit(failures)
+
+
+def run_tests(test_last_n_apps=-1):
+    """
+    Like run_tests_for but assumes that settings_module_name is
+    "test_project.settings"
+    """
+    return run_tests_for("test_project.settings", test_last_n_apps)
